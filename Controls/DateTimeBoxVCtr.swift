82fea9512d057572d61c6d754aae123bfdc5ae5c
Daniel Scheibe <daniel.scheibe@me.com>
2017-04-30 00:44:22 +0200

    Update DateTimeBoxVCtr.swift

--- Controls/DateTimeBoxVCtr.swift
+++ Controls/DateTimeBoxVCtr.swift
@@ -78,7 +78,10 @@ import UIKit
 			self.pickerView.minimumDate = min
 		}
 
-		// self.pickerView.datePickerMode = (self.refDateTimeBox?.delegateForDateTimeBox?.dateTimeBoxType(self.refDateTimeBox!))!
+		if let type = self.refDateTimeBox?.delegateForDateTimeBox?.dateTimeBoxType(self.refDateTimeBox!) {
+            
+            		self.pickerView.datePickerMode = type
+        	}
 	}
 
 	override open func didReceiveMemoryWarning() {
